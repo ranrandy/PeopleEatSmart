21221c7d60c7b93ab72d9c9b487a42b6d822aaee
acyin2 <35507797+acyin2@users.noreply.github.com>
2021-07-22 16:40:28 -0500

    jjl

--- RandyEatSmart/urls.py
+++ RandyEatSmart/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register(r'randy', RandyViewSet, basename = "test")
-router.register(r'robert', RobertViewSet, basename = "test2")
+router.register(r'randy', RandyViewSet, basename="test")
+router.register(r'robert', RobertViewSet, basename="test2")
 # router.register(r'robert/<int:key>', RobertViewSet, basename = "test2")
 
 urlpatterns = router.urls
